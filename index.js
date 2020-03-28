aaf3266bc58266719946750f87daaf1a3cdaa23d
allison_johnson <allison.cuttler@gmail.com>
2020-03-28 14:10:40 -0700

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function fetchBooks() {
-
-}
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    //debugger
+    return renderBooks(json);
+  })
+}// function fetchBooks
 
 function renderBooks(json) {
   const main = document.querySelector('main')
@@ -9,8 +15,12 @@ function renderBooks(json) {
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
   })
-}
+}// function renderBooks
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
-})
+})//event listener for DOMContentLoaded
+
+
+
+

4c354a2918579998a5cef71cc78c9a69d18f11ad
Adrian082818 <mohabiradrian@gmail.com>
2020-03-30 15:35:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function fetchBooks() {
-
+return fetch('https://anapioficeandfire.com/api/books')
+.then(resp => resp.json())
+// return resp.json()
+.then(json => renderBooks(json))
 }
 
 function renderBooks(json) {
